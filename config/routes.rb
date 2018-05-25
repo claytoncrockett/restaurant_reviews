b86718991dcac5d6de044fb89c8c21c85c361380
Clayton Crockett <claytoncrockett@gmail.com>
2018-05-25 16:41:49 -0600

    added some really cool stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'static/home'
+  get 'static/about'
+    root 'static#home'
+  
+    get '/about', to: 'static#about'
+  
+  resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
